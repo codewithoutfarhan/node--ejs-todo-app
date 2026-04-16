0f298588cccb7e37848effd3ea97f31d18833404
codewithoutfarhan <fparwez83@gmail.com>
2026-04-16 11:02:59 +0530

    final working code

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const path = require("path");
-
-app.set("views", path.join(__dirname, "views"));
-
 const express = require('express');
 const app = express();
 const path = require("path");
 const fs = require("fs");
 
+const PORT = process.env.PORT || 3000;
+
+// MIDDLEWARE
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // HOME
 app.get("/", function(req, res){
-    fs.readdir(path.join(__dirname, "files"), function(err, files){
+    const dirPath = path.join(__dirname, "files");
+
+    fs.readdir(dirPath, function(err, files){
         if(err){
-            return res.send("Error");
+            return res.send("Error reading files: " + err.message);
         }
         res.render("index", { files });
     });
@@ -29,7 +32,8 @@ app.post("/create", function(req, res){
 
     const filePath = path.join(__dirname, "files", title + ".txt");
 
-    fs.writeFile(filePath, details || "", function(){
+    fs.writeFile(filePath, details || "", function(err){
+        if(err) return res.send(err.message);
         res.redirect("/");
     });
 });
@@ -39,6 +43,7 @@ app.get("/file/:filename", function(req, res){
     const filePath = path.join(__dirname, "files", req.params.filename);
 
     fs.readFile(filePath, "utf-8", function(err, data){
+        if(err) return res.send(err.message);
         res.render("read", { fileName: req.params.filename, data });
     });
 });
@@ -48,6 +53,7 @@ app.get("/edit/:filename", function(req, res){
     const filePath = path.join(__dirname, "files", req.params.filename);
 
     fs.readFile(filePath, "utf-8", function(err, data){
+        if(err) return res.send(err.message);
         res.render("edit", { fileName: req.params.filename, data });
     });
 });
@@ -56,9 +62,11 @@ app.get("/edit/:filename", function(req, res){
 app.post("/update/:filename", function(req, res){
     const filePath = path.join(__dirname, "files", req.params.filename);
 
-    fs.writeFile(filePath, req.body.details, function(){
+    fs.writeFile(filePath, req.body.details, function(err){
+        if(err) return res.send(err.message);
         res.redirect("/file/" + req.params.filename);
     });
 });
 
-app.listen(3000, () => console.log("server running"));
+// START SERVER
+app.listen(PORT, () => console.log("Server running on port " + PORT));
